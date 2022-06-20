28330c2f7330bb565723866a9a60c8c9e23602c8
Stagiaires11 <Stagiaires11P@ad.fms-ea.com>
2022-06-20 11:30:23 +0200

    first commit

---
+++ src/main/java/com/fms/maboutiqueenligne/MaBoutiqueEnLigneApplication.java
@@ -0,0 +1,13 @@
+package com.fms.maboutiqueenligne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaBoutiqueEnLigneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaBoutiqueEnLigneApplication.class, args);
+	}
+
+}

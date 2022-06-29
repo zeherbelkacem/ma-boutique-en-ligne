0844f1b85a9a683d750b8bddaf54b791ccd8f2be
Delmerie <j.delmerie@live.fr>
2022-06-29 10:37:52 +0200

    affiche liste des dernières commandes

--- src/main/java/com/fms/springEx1/Controller/CartController.java
+++ src/main/java/com/fms/springEx1/Controller/CartController.java
@@ -4,10 +4,8 @@ import java.security.Principal;
 import java.util.Date;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
--- src/main/java/com/fms/springEx1/Controller/OrdersController.java
+++ src/main/java/com/fms/springEx1/Controller/OrdersController.java
@@ -1,15 +1,22 @@
 package com.fms.springEx1.Controller;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fms.springEx1.Entities.Order;
+import com.fms.springEx1.Security.UserService;
+import com.fms.springEx1.Security.Uuser;
 import com.fms.springEx1.Service.OrderService;
 
 @Controller
@@ -17,6 +24,9 @@ public class OrdersController {
 
 	@Autowired
 	private OrderService orderService;
+	
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping("saveOrder")
 	public String saveOrder(@RequestParam(name = "customerId", defaultValue = "") Long customerId,
@@ -31,7 +41,6 @@ public class OrdersController {
 	public String adminOrders(Model model, @RequestParam(name = "orderId", defaultValue = "") Long orderId,
 			@RequestParam(name = "page", defaultValue = "0") int page,
 			@RequestParam(name = "size", defaultValue = "4") int size
-
 	) {
 		if (orderId != null)
 			orderService.loadInvoice(orderId);
@@ -43,5 +52,25 @@ public class OrdersController {
 		model.addAttribute("listOf", "List of Orders");
 		model.addAttribute("listOrders", ordersPages);
 		return "orders";
+	}
+
+	@GetMapping("validateOrder")
+	public String validateOrder(@RequestParam(name = "customerId", defaultValue = "") Long customerId,
+			RedirectAttributes model) {
+		model.addAttribute("customerId", customerId);
+		orderService.saveOrder(customerId);
+		return "redirect:/listOrders";
+	}
+	
+	@GetMapping("listOrders")
+	public String listOrders(Model model, Principal principal,
+			@RequestParam(name = "customerId", defaultValue = "") Long customerId) {
+		Uuser user = userService.findUuserByUserName(principal.getName());
+		List<Order> orders = new ArrayList<>();
+		 user.getCustomers().forEach(c -> {
+			orders.addAll(c.getOrders());
+		});
+		model.addAttribute("orders", orders);
+		return "customerOrders";
 	}
 }

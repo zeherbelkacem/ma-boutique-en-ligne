8465eafc79b6d1b8ee693e021f001f6f3b18d1dc
belkacem <59140588+zeherbelkacem@users.noreply.github.com>
2022-07-01 15:18:28 +0200

    Update SecurityConfig.java

--- src/main/java/com/fms/springEx1/Security/SecurityConfig.java
+++ src/main/java/com/fms/springEx1/Security/SecurityConfig.java
@@ -16,6 +16,40 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+/**
+ * 
+ * @author groupe Caroline, Delmerie et Belkacem
+ * 
+ *         ***************** Mise en place de spring security ********** 1- On
+ *         crée une class (securityConfig) qui hérite de la classe
+ *         "WebSecurityConfigurerAdapter" de sring secutity 2- On active spring
+ *         security avec le annotation @Configuration et @EnableWebSecurity pour
+ *         désactiver la security de spring par défaut et utiliser une sécurité
+ *         ciblée, ici UserDetails 3- On charge deux methodes de
+ *         "WebSecurityConfigurerAdapter"
+ * 
+ *         3-1- configure(AuthenticationManagerBuilder auth): pour
+ *         l'authentification et le choix de l'authentification - Pour le choix
+ *         de l'authentification, on choisit UsersDetails en faisant appel à
+ *         "userDetailsService" . - userDetailsService nous fournit une méthode
+ *         (loadUserByUsername(String username)) qui récupère les données du
+ *         formulaire d'authentification de l'utilisateur - Avec les données
+ *         récuperées (userName et password), on fait appel à la base de donnée
+ *         users (JPA) et chercher le userName correspondant - Si le userName
+ *         existe, on charge le User (return new User(uuser.getUserName(),
+ *         uuser.getPassword(), uuser.getActive(), true, true, true,
+ *         grantedAuthorities);) de spring boot security avec les donnée des
+ *         (password, roles, active...) l'utilisateur chargé depuis la bd afin
+ *         qu'il puisse verifier l'authenticité et le droit d'accès de
+ *         l'utilisateur
+ * 
+ *         3-2- protected void configure(HttpSecurity http): pour la gestion de
+ *         la securité des requette http - Le formulaire de login à utiliser -
+ *         la protection des routers selon les role utilisateurs - ....
+ *
+ */
+
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
